161b9dacf59ed0421fe11b66e0a1db51c80c9b29
deikdesign <deikdesign@gmail.com>
2020-11-11 21:06:13 +0200

    Put an output

--- bin/main.rb
+++ bin/main.rb
@@ -4,6 +4,7 @@ File.open('./bin/test.css', 'r') do |f|
   data = f.read
   linter = Linter.new(data)
   errors = linter.lint
+  puts 'There are no issues detected' if errors.length <= 0
   errors.each do |key, value|
     value.each do |error|
       puts "Line #{key}: #{error}"
